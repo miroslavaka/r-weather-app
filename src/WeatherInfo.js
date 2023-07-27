8d58f0811a3da0bea6feec3eddb1fea6e74af3a4
miroslavaka <kacmarova.miroslava@gmail.com>
2023-07-27 16:34:27 +0200

    celsius and fahrenheit calculation

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import FormattedDate from './FormattedDate';
 import WeatherIcon from './WeatherIcon';
+import WeatherTemperature from './WeatherTemperature';
 
 export default function WeatherInfo(props) {
+  console.log('temperature: ', props.data.temperature);
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
@@ -25,15 +27,14 @@ export default function WeatherInfo(props) {
               /> */}
             </div>
             <div className="float-left">
-              <span className="temperature">{props.data.temperature}</span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind}km/h</li>
+            <li>Wind: {Math.round(props.data.wind)}km/h</li>
           </ul>
         </div>
       </div>
